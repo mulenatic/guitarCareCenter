93031981432302618ad83d6a4d88af70229ac0f0
Thomas Kaczmarek <mulenatic@gmail.com>
2016-09-15 21:06:32 +0200

    Guitars created in the GuitarCreator are shown in List.

--- client/src/components/GuitarCreator/GuitarCreator.ts
+++ client/src/components/GuitarCreator/GuitarCreator.ts
@@ -2,6 +2,8 @@ import * as angular from "angular";
 
 import { Guitar } from "../../domain/Guitar";
 
+import GuitarServiceModule, {IGuitarService} from "../../services/GuitarService";
+
 const moduleName = "guitarCareCenterApp.guitarCreationComponent";
 export default moduleName;
 
@@ -9,12 +11,17 @@ class GuitarCreator {
 
     guitar: Guitar;
 
-    constructor() {
+    constructor(private guitarService: IGuitarService) {
         this.guitar = new Guitar();
     }
 
     resetForm(): void {
         this.guitar = new Guitar();
+    }
+
+    add() {
+        this.guitarService.add(this.guitar);
+        this.resetForm();
     }
 
 }
